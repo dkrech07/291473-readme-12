403e5dc5d9c6782b1515d8f39f64c1be6e13715a
Дмитрий Кречетников <dkrech07@gmail.com>
2020-10-18 00:56:19 +0500

    Вносит правки

--- index.php
+++ index.php
@@ -5,63 +5,29 @@ require_once('helpers.php');
 $is_auth = rand(0, 1);
 $user_name = 'Дмитрий';
 
-// $posts = [
-//     [
-//     'title' =>'Полезный пост про Байкал',
-//     'type' =>'post-text',
-//     'description' => 'Озеро Байкал - огромное древнее озеро в горах Сибири к северу от монгольской границы. Оно считается самым глубоким в мире. Его средняя глубина - 744 метра. Двадцать первого ноября уровень воды в Байкале составил почти 456 метров.Проблема снижающегося уровня воды в Байкале неоднократно поднималась. Сейчас он меньше порогового значения на шесть сантиметров. Для остановки оттока воды из озера власти предпринимают экономические решения. За последние 60 лет колебания уровня воды в Байкале, близкие к критической отметке, наблюдались 15 раз. Так, в 1981 году уровень Байкала опустился на 70 сантиметров ниже критической отметки.',
-//     'user_name' => 'Лариса',
-//     'avatar' => 'userpic-larisa-small.jpg',
-//   ],
-//   [
-//     'title' =>'Цитата',
-//     'type' =>'post-quote',
-//     'description' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
-//     'user_name' => 'Лариса',
-//     'avatar' => 'userpic-larisa-small.jpg',
-//   ],
-//   [
-//     'title' =>'Игра престолов',
-//     'type' =>'post-text',
-//     'description' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
-//     'user_name' => 'Владик',
-//     'avatar' => 'userpic.jpg',
-//   ],
-//   [
-//     'title' =>'Наконец, обработал фотки!',
-//     'type' =>'post-photo',
-//     'description' => 'rock-medium.jpg',
-//     'user_name' => 'Виктор',
-//     'avatar' => 'userpic-mark.jpg',
-//   ],
-//   [
-//     'title' =>'Моя мечта',
-//     'type' =>'post-photo',
-//     'description' => 'coast-medium.jpg',
-//     'user_name' => 'Лариса',
-//     'avatar' => 'userpic-larisa-small.jpg',
-//   ],
-//   [
-//     'title' =>'Лучшие курсы',
-//     'type' =>'post-link',
-//     'description' => 'www.htmlacademy.ru',
-//     'user_name' => 'Владик',
-//     'avatar' => 'userpic.jpg',
-//   ],
-// ];
-
 $con = mysqli_connect("localhost", "root", "root","readme");
-
 $content_types_select = 'SELECT * FROM content_types';
 $posts_select = 'SELECT p.*, u.login, u.avatar, ct.type_name, ct.class_name FROM posts p INNER JOIN users u ON u.id = p.post_author_id INNER JOIN content_types ct ON ct.id = p.content_type_id ORDER BY p.views DESC';
 
-$content_types_query = mysqli_query($con, $content_types_select);
-$posts_query = mysqli_query($con, $posts_select);
+function select_query($con, $sql) {
+  mysqli_set_charset($con, "utf8");
 
-$content_types = mysqli_fetch_all($content_types_query, MYSQLI_ASSOC);
-$posts = mysqli_fetch_all($posts_query, MYSQLI_ASSOC);
-// print_r($posts);
-print_r($content_types);
+  if (!$con) {
+    echo ("Ошибка подключения: " . mysqli_connect_error());
+    return null;
+  }
+
+  $result = mysqli_query($con, $sql);
+  if (!$result) {
+    return null;
+  }
+
+  return mysqli_fetch_all($result, MYSQLI_ASSOC);
+}
+
+
+$content_types = select_query($con, $content_types_select);
+$posts = select_query($con, $posts_select);
 
 function crop_text($text, $characters_count = 300) {
   if (mb_strlen($text) <= $characters_count) {
