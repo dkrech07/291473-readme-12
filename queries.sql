c7ffdbceabac70b521c12189d243e5880e0d277b
Дмитрий Кречетников <dkrech07@gmail.com>
2020-10-17 20:08:30 +0500

    Исправляет запросы для добавления данных в БД

--- queries.sql
+++ queries.sql
@@ -1,9 +1,9 @@
 -- Добавляет список типов контента для поста;
-INSERT INTO content_types SET type_name = 'Текст', class_name = 'text';
-INSERT INTO content_types SET type_name = 'Цитата', class_name = 'quote';
-INSERT INTO content_types SET type_name = 'Картинка', class_name = 'photo';
-INSERT INTO content_types SET type_name = 'Видео', class_name = 'video';
-INSERT INTO content_types SET type_name = 'Ссылка', class_name = 'link';
+INSERT INTO content_types (type_name, class_name) VALUES ('Текст', 'text');
+INSERT INTO content_types (type_name, class_name) VALUES ('Цитата', 'quote');
+INSERT INTO content_types (type_name, class_name) VALUES ('Картинка', 'photo');
+INSERT INTO content_types (type_name, class_name) VALUES ('Видео', 'video');
+INSERT INTO content_types (type_name, class_name) VALUES ('Ссылка', 'link');
 
 -- Добавляет пользователей;
 -- password_hash('Q4B4e6Ap', PASSWORD_DEFAULT); // $2y$10$feI4UmS8vwRUqjGuvBkNheLQ4eL5KI4PW8YN11SmhzRi9ELC0ehUq
@@ -11,25 +11,25 @@ INSERT INTO content_types SET type_name = 'Ссылка', class_name = 'link';
 -- password_hash('MKLVL4Ce', PASSWORD_DEFAULT); // $2y$10$hN2SZp43qwJdZQk8QwIGtudREYDU/2lRCfhhmVcEwL6nV/OJaIVI6
 -- password_hash('tM9EqxSD', PASSWORD_DEFAULT); // $2y$10$6Ekqhbl/mh/MyznsP3.CiuX/OIHhe0N/vdKMhVgBdNw5uQU1QjtVK
 -- password_hash('KtG2brMZ', PASSWORD_DEFAULT); // $2y$10$Ai.9TbowPmD04wF.W5KxOeeeos1w4T3TiJZCoh3tn5ilnLt1VrgB
-INSERT INTO users SET date_add = '2020-01-16 16:30:25', email = 'larisa@mail.ru', login = 'larisa', password = '$2y$10$feI4UmS8vwRUqjGuvBkNheLQ4eL5KI4PW8YN11SmhzRi9ELC0ehUq', avatar='img/userpic-larisa-small.jpg';
-INSERT INTO users SET date_add = '2020-02-15 15:45:20', email = 'vladik@mail.ru', login = 'vladik', password = '$2y$10$7uOaL/PzYWwnhT9Ly2mz9.6tjORSkt7D2RZZ/ZO8mlozZnpLcy9Dm', avatar='img/userpic.jpg';
-INSERT INTO users SET date_add = '2020-05-16 13:50:30', email = 'viktor@mail.ru', login = 'viktor', password = '$2y$10$hN2SZp43qwJdZQk8QwIGtudREYDU/2lRCfhhmVcEwL6nV/OJaIVI6', avatar='img/userpic-mark.jpg';
-INSERT INTO users SET date_add = '2020-07-16 12:30:30', email = 'tanya@mail.ru', login = 'tanya', password = '$2y$10$6Ekqhbl/mh/MyznsP3.CiuX/OIHhe0N/vdKMhVgBdNw5uQU1QjtVK', avatar='img/userpic-tanya.jpg';
-INSERT INTO users SET date_add = '2020-08-16 05:55:30', email = 'petro@mail.ru', login = 'petro', password = '$2y$10$Ai.9TbowPmD04wF.W5KxOeeeos1w4T3TiJZCoh3tn5ilnLt1VrgB', avatar='img/userpic-petro.jpg';
+INSERT INTO users (date_add, email, login, password, avatar) VALUES ('2020-01-16 16:30:25', 'larisa@mail.ru', 'larisa', '$2y$10$feI4UmS8vwRUqjGuvBkNheLQ4eL5KI4PW8YN11SmhzRi9ELC0ehUq', 'img/userpic-larisa-small.jpg');
+INSERT INTO users (date_add, email, login, password, avatar) VALUES ('2020-02-15 15:45:20', 'vladik@mail.ru', 'vladik', '$2y$10$7uOaL/PzYWwnhT9Ly2mz9.6tjORSkt7D2RZZ/ZO8mlozZnpLcy9Dm', 'img/userpic.jpg');
+INSERT INTO users (date_add, email, login, password, avatar) VALUES ('2020-05-16 13:50:30', 'viktor@mail.ru', 'viktor', '$2y$10$hN2SZp43qwJdZQk8QwIGtudREYDU/2lRCfhhmVcEwL6nV/OJaIVI6', 'img/userpic-mark.jpg');
+INSERT INTO users (date_add, email, login, password, avatar) VALUES ('2020-07-16 12:30:30', 'tanya@mail.ru', 'tanya', '$2y$10$6Ekqhbl/mh/MyznsP3.CiuX/OIHhe0N/vdKMhVgBdNw5uQU1QjtVK', 'img/userpic-tanya.jpg');
+INSERT INTO users (date_add, email, login, password, avatar) VALUES ('2020-08-16 05:55:30', 'petro@mail.ru', 'petro', '$2y$10$Ai.9TbowPmD04wF.W5KxOeeeos1w4T3TiJZCoh3tn5ilnLt1VrgB', 'img/userpic-petro.jpg');
 
 -- Добавляет список постов;
-INSERT INTO posts SET date_add = '2020-10-16 11:31:26', title = 'Цитата', content = 'Мы в жизни любим только раз, а после ищем лишь похожих', views = '2', post_author_id = '1', content_type_id = '2';
-INSERT INTO posts SET date_add = '2020-10-10 16:31:26', title = 'Игра престолов', content = 'Не могу дождаться начала финального сезона своего любимого сериала!', views = '5', post_author_id = '2', content_type_id = '1';
-INSERT INTO posts SET date_add = '2020-10-09 16:31:26', title = 'Наконец, обработал фотки!', content = 'rock-medium.jpg', image = 'img/rock-medium.jpg', views = '99', post_author_id = '3', content_type_id = '3';
-INSERT INTO posts SET date_add = '2020-02-16 16:31:26', title = 'Моя мечта', content = 'coast-medium.jpg', image = 'img/coast-medium.jpg', views = '3', post_author_id = '1', content_type_id = '3';
-INSERT INTO posts SET date_add = '2020-06-16 16:31:26', title = 'Лучшие курсы', content = 'www.htmlacademy.ru', link = 'www.htmlacademy.ru', views = '2', post_author_id = '2', content_type_id = '5';
+INSERT INTO posts (date_add, title, content, views, post_author_id, content_type_id) VALUES ('2020-10-16 11:31:26', 'Цитата', 'Мы в жизни любим только раз, а после ищем лишь похожих', 2, 1, 2);
+INSERT INTO posts (date_add, title, content, views, post_author_id, content_type_id) VALUES ('2020-10-10 16:31:26', 'Игра престолов', 'Не могу дождаться начала финального сезона своего любимого сериала!', 5, 2, 1);
+INSERT INTO posts (date_add, title, content, image, views, post_author_id, content_type_id) VALUES ('2020-10-09 16:31:26', 'Наконец, обработал фотки!', 'rock-medium.jpg', 'img/rock-medium.jpg', 99, 3, 3);
+INSERT INTO posts (date_add, title, content, image, views, post_author_id, content_type_id) VALUES ('2020-02-16 16:31:26', 'Моя мечта', 'coast-medium.jpg', 'img/coast-medium.jpg', 3, 1, 3);
+INSERT INTO posts (date_add, title, content, link, views, post_author_id, content_type_id) VALUES ('2020-06-16 16:31:26', 'Лучшие курсы', 'www.htmlacademy.ru', 'www.htmlacademy.ru', 2, 2, 5);
 
 -- Добавляет комментарии к постам;
-INSERT INTO comments SET date_add = '2020-10-16 11:35:26', content = 'Красивые стихи... Это Есенин?', comment_author_id = '2', post_id = '1';
-INSERT INTO comments SET date_add = '2020-10-16 11:35:26', content = 'Ага, я тоже жду новый сезон', comment_author_id = '3', post_id = '2';
-INSERT INTO comments SET date_add = '2020-10-16 11:35:26', content = 'Очень неплохо. На какую камеру снимал?', comment_author_id = '1', post_id = '3';
-INSERT INTO comments SET date_add = '2020-10-16 11:35:26', content = 'Горизонт завален! У меня бабушка лучше фоткает!', comment_author_id = '4', post_id = '3';
-INSERT INTO comments SET date_add = '2020-10-16 11:35:26', content = 'Красиво)))', comment_author_id = '2', post_id = '4';
+INSERT INTO comments (date_add, content, comment_author_id, post_id) VALUES ('2020-10-16 11:35:26', 'Красивые стихи... Это Есенин?', 2, 1);
+INSERT INTO comments (date_add, content, comment_author_id, post_id) VALUES ('2020-10-16 11:35:26', 'Ага, я тоже жду новый сезон', 3, 2);
+INSERT INTO comments (date_add, content, comment_author_id, post_id) VALUES ('2020-10-16 11:35:26', 'Очень неплохо. На какую камеру снимал?', 1, 3);
+INSERT INTO comments (date_add, content, comment_author_id, post_id) VALUES ('2020-10-16 11:35:26', 'Горизонт завален! У меня бабушка лучше фоткает!', 4, 3);
+INSERT INTO comments (date_add, content, comment_author_id, post_id) VALUES ('2020-10-16 11:35:26', 'Красиво)))', 2, 4);
 
 -- Получает список постов с сортировкой по популярности и вместе с именами авторов и типом контента;
 SELECT p.*, u.login, ct.type_name FROM posts p INNER JOIN users u ON u.id = p.post_author_id INNER JOIN content_types ct ON ct.id = p.content_type_id ORDER BY p.views DESC;
