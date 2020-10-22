c8b409dfedb4cbec52149dc57034ed3f8eb21aee
Дмитрий Кречетников <dkrech07@gmail.com>
2020-10-23 00:31:30 +0500

    Добавляет вывод 404 страницы

--- post.php
+++ post.php
@@ -5,12 +5,20 @@ require_once('functions.php');
 $is_auth = rand(0, 1);
 $user_name = 'Дмитрий';
 
+
+if (!isset($_GET['id'])) {
+    echo('404 Запрошенная страница не найдена');
+    http_response_code(404);
+    exit();
+}
+print($_GET['id']);
+
 $current_post_id = filter_input(INPUT_GET, 'id');
 $con = mysqli_connect('localhost', 'root', 'root','readme') or trigger_error('Ошибка подключения: '.mysqli_connect_error(), E_USER_ERROR);
 $post = select_query($con, 'SELECT p.*, u.login, u.date_add, u.avatar, ct.type_name, ct.class_name FROM posts p INNER JOIN users u ON u.id = p.post_author_id INNER JOIN content_types ct ON ct.id = p.content_type_id WHERE p.id = ' . $current_post_id, 'single');
 $registration_time = get_post_interval($post['date_add'], 'на сайте');
 
-print_r($post);
+// print_r($post);
 
 $post_content = include_template('post-' . $post['class_name'] .'.php', ['post' => $post, 'registration_time' => $registration_time,]);
 
