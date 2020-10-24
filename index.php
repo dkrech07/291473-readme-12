4c30b95b0cf5c1ea3457718650cc92dad1bb465c
Дмитрий Кречетников <dkrech07@gmail.com>
2020-10-24 14:01:27 +0500

    Реализует фильтрацию постов

--- index.php
+++ index.php
@@ -8,10 +8,18 @@ $user_name = 'Дмитрий';
 
 $con = mysqli_connect('localhost', 'root', 'root','readme') or trigger_error('Ошибка подключения: '.mysqli_connect_error(), E_USER_ERROR);
 $content_types = select_query($con, 'SELECT * FROM content_types');
-$posts = select_query($con, 'SELECT p.*, u.login, u.avatar, ct.type_name, ct.class_name FROM posts p INNER JOIN users u ON u.id = p.post_author_id INNER JOIN content_types ct ON ct.id = p.content_type_id ORDER BY p.views DESC');
+
+if (isset($_GET['post_type'])) {
+  $post_type = filter_input(INPUT_GET, 'post_type');
+  $post_type_query = 'WHERE p.content_type_id = ' . $post_type;
+} else {
+  $post_type = null;
+  $post_type_query = '';
+}
+
+$posts = select_query($con, 'SELECT p.*, u.login, u.avatar, ct.type_name, ct.class_name FROM posts p INNER JOIN users u ON u.id = p.post_author_id INNER JOIN content_types ct ON ct.id = p.content_type_id ' . $post_type_query . ' ORDER BY p.views DESC');
 
 $current_sorting_type = filter_input(INPUT_GET, 'sorting-type');
-$post_type = filter_input(INPUT_GET, 'post_type');
 
 $page_content = include_template('main.php', [
     'posts' => $posts,
