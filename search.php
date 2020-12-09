8e88b65207d95fba55ead9442bb762d740e35b1c
Дмитрий Кречетников <dkrech07@gmail.com>
2020-12-09 22:24:41 +0500

    Выводит посты на странице поиска

--- search.php
+++ search.php
@@ -11,18 +11,18 @@ $avatar = $_SESSION['user']['avatar'];
 $search_posts = [];
 mysqli_query($con, "CREATE FULLTEXT INDEX posts_search ON posts(title, content)");
 $search = filter_input(INPUT_GET, 'q') ?? '';
-print($search);
 
 if ($search) {
-  $search_query = "SELECT * FROM posts WHERE MATCH(title, content) AGAINGS(?)";
+  $search_query = "SELECT p.*, u.login, u.date_add, u.avatar, ct.type_name, ct.class_name FROM posts p INNER JOIN users u ON u.id = p.post_author_id INNER JOIN content_types ct ON ct.id = p.content_type_id WHERE MATCH(title, content) AGAINST(?)";
   $stmt = mysqli_prepare($con, $search_query);
   mysqli_stmt_bind_param($stmt, 's', $search);
   mysqli_stmt_execute($stmt);
   $search_result = mysqli_stmt_get_result($stmt);
-  $search_posts = mysqli_fetch_all($search_result, MYSQL_ASSOC);
+  $search_posts = mysqli_fetch_all($search_result, MYSQLI_ASSOC);
 }
 
 $page_content = include_template('search.php', [
+  'search' => $search,
   'search_posts' => $search_posts,
 ]);
 
--- templates/search.php
+++ templates/search.php
@@ -14,25 +14,69 @@
         <div class="search__results-wrapper">
           <div class="container">
             <div class="search__content">
-            <?php foreach ($subscription_posts as $post_number => $post): ?>
-              <article class="search__post post post-photo">
+            <?php foreach ($search_posts as $post_number => $post): ?>
+              <article class="search__post post post-<?= $post['class_name'] ?>">
                 <header class="post__header post__author">
                   <a class="post__author-link" href="#" title="Автор">
                     <div class="post__avatar-wrapper">
-                      <img class="post__author-avatar" src="img/userpic-elvira.jpg" alt="Аватар пользователя" width="60" height="60">
+                      <img class="post__author-avatar" src="<?= $post['avatar'] ?>" alt="Аватар пользователя" width="60" height="60">
                     </div>
                     <div class="post__info">
-                      <b class="post__author-name">Эльвира Хайпулинова</b>
-                      <span class="post__time">15 минут назад</span>
+                      <b class="post__author-name"><?= $post['login'] ?></b>
+                      <span class="post__time"><?= get_post_interval($post['date_add'], 'назад') ?></span>
                     </div>
                   </a>
                 </header>
                 <div class="post__main">
-                  <h2><a href="#">Наконец, обработала фотки!</a></h2>
-                  <div class="post-photo__image-wrapper">
-                    <img src="img/rock.jpg" alt="Фото от пользователя" width="760" height="396">
-                  </div>
+                <?php if ($post['class_name'] == "quote"): ?>
+                <blockquote>
+                    <?= crop_text($post['content']) ?>
+                    <?php if ($post['quote_author']): ?>
+                      <cite><?= $post['quote_author'] ?></cite>
+                    <?php else: ?>
+                      <cite>Неизвестный Автор</cite>
+                    <?php endif; ?>
+                </blockquote>
+
+                <?php elseif ($post['class_name'] == "link"): ?>
+                <div class="post-link__wrapper">
+                    <a class="post-link__external" href="<?= $post['link'] ?>" title="Перейти по ссылке">
+                        <div class="post-link__info-wrapper">
+                            <div class="post-link__icon-wrapper">
+                                <img src="https://www.google.com/s2/favicons?domain=vitadental.ru" alt="Иконка">
+                            </div>
+                            <div class="post-link__info">
+                                <h3><?= $post['title'] ?></h3>
+                            </div>
+                        </div>
+                        <span><?= $post['content'] ?></span>
+                    </a>
                 </div>
+
+                <?php elseif ($post['class_name'] == "photo"): ?>
+                <div class="post-photo__image-wrapper">
+                    <img src="<?= $post['image'] ?>" alt="Фото от пользователя" width="360" height="240">
+                </div>
+
+                <?php elseif ($post['class_name'] == "video"): ?>
+                <div class="post-video__block">
+                    <div class="post-video__preview">
+                        <?=embed_youtube_cover($post['video']); ?>
+                        <img src="img/coast-medium.jpg" alt="Превью к видео" width="360" height="188">
+                    </div>
+                    <a href="post-details.html" class="post-video__play-big button">
+                        <svg class="post-video__play-big-icon" width="14" height="14">
+                            <use xlink:href="#icon-video-play-big"></use>
+                        </svg>
+                        <span class="visually-hidden">Запустить проигрыватель</span>
+                    </a>
+                </div>
+
+                <?php elseif ($post['class_name'] == "text"): ?>
+                <?= crop_text($post['content']) ?>
+
+                <?php endif; ?>
+            </div>
                 <footer class="post__footer post__indicators">
                   <div class="post__buttons">
                     <a class="post__indicator post__indicator--likes button" href="#" title="Лайк">
