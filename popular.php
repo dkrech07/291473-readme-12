f2fa8cc80a491112d241c6f53ed0019dcf5e0800
Дмитрий Кречетников <dkrech07@gmail.com>
2021-01-13 16:45:46 +0500

    Начинает реализовывать пагинацию

--- popular.php
+++ popular.php
@@ -40,22 +40,37 @@ if ($sorting_type == 'popular') {
 }
 
 // Получает список постов (в зависимости от выбранного типа контента);
-$posts = select_query($con, 'SELECT p.*, u.login, u.*, ct.type_name, ct.class_name FROM posts p INNER JOIN users u ON u.id = p.post_author_id INNER JOIN content_types ct ON ct.id = p.content_type_id ' . $post_type_query . ' ' . $sorting_order);
+//$posts = select_query($con, 'SELECT p.*, u.login, u.*, ct.type_name, ct.class_name FROM posts p INNER JOIN users u ON u.id = p.post_author_id INNER JOIN content_types ct ON ct.id = p.content_type_id ' . $post_type_query . ' ' . $sorting_order);
 
+$page = 0;
+$current_page = 1;
+$next_page = 0;
+$limit = 9;
 
 // Вычислить количество постов;
+// Вычислить количество страниц;
 $posts_count = select_query($con, "SELECT COUNT(*) FROM posts", 'row');
 
 if ($posts_count > 9) {
-    $posts = select_query($con, 'SELECT p.*, u.login, u.*, ct.type_name, ct.class_name FROM posts p INNER JOIN users u ON u.id = p.post_author_id INNER JOIN content_types ct ON ct.id = p.content_type_id ' . $post_type_query . ' ' . $sorting_order);
+    $pages_count = ceil($posts_count/$limit);
+    if ($posts_count%$limit !== 0) {
+        $pages_count++;
+    }
 
+    $page = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT);
+    if (isset($page)) {
+        $posts = select_query($con, 'SELECT p.*, u.login, u.*, ct.type_name, ct.class_name FROM posts p INNER JOIN users u ON u.id = p.post_author_id INNER JOIN content_types ct ON ct.id = p.content_type_id ' . $post_type_query . ' ' . $sorting_order . ' LIMIT ' . $limit . ' OFFSET ' . $page);
+        print($page);
+    } else {
+        $page = 0;
+        $posts = select_query($con, 'SELECT p.*, u.login, u.*, ct.type_name, ct.class_name FROM posts p INNER JOIN users u ON u.id = p.post_author_id INNER JOIN content_types ct ON ct.id = p.content_type_id ' . $post_type_query . ' ' . $sorting_order . ' LIMIT ' . $limit . ' OFFSET ' . $page);
+
+    }
 }
 
 // Если постов больше 9, вывести 9 постов и кнопки пагинации;
 // Вычислить количество кнопок пагинации;
 // Сгенерировать ссылки на кнопках пагинации;
-$posts_2 = select_query($con, "SELECT *FROM posts LIMIT 9 OFFSET 0");
-print_r($posts_count);
 
 if (!$posts) {
     open_404_page($user_name, $avatar);
@@ -70,6 +85,8 @@ $page_content = include_template('main.php', [
     'post_type' => $post_type,
     'sorting_type' => $sorting_type,
     'sorting_direction' => $sorting_direction,
+    'pages_count' => $pages_count,
+    'current_page' => $current_page,
 ]);
 
 $layout_content = include_template('layout.php', [
--- templates/main.php
+++ templates/main.php
@@ -1,3 +1,7 @@
+<?php
+print($pages_count);
+?>
+
 <div class="container">
     <h1 class="page__title page__title--popular">Популярное</h1>
 </div>
@@ -155,6 +159,16 @@
         </article>
         <?php endforeach; ?>
     </div>
+    <?php if($pages_count > 1): ?>
+    <div class="popular__page-links">
+        <?php if($current_page > 1): ?>
+            <a class="popular__page-link popular__page-link--prev button button--gray" href="/popular.php?page=<?= $page + 1 ?>">Предыдущая страница</a>
+        <?php endif; ?>
+        <?php if($current_page < $pages_count): ?>
+            <a class="popular__page-link popular__page-link--next button button--gray" href="/popular.php?page=<?= $page + 1 ?>">Следующая страница</a>
+        <?php endif; ?>
+    </div>
+    <?php endif; ?>
 </div>
 <script src="js/custom.js"></script>
 <script src="js/custom-sort.js"></script>
