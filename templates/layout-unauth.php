a9129b69b4bfd84cda9e6465b20684d54e1bc548
Дмитрий Кречетников <dkrech07@gmail.com>
2021-02-18 14:25:32 +0500

    Вносит правки

--- templates/layout-unauth.php
+++ templates/layout-unauth.php
@@ -35,7 +35,7 @@ $login = $_POST['login'] ?? '';
             </p>
             <ul class="header__user-nav">
               <li>
-                <a class="header__user-button header__register-button button button--transparent" href="registration.html">Регистрация</a>
+                <a class="header__user-button header__register-button button button--transparent" href="registration.php">Регистрация</a>
               </li>
             </ul>
           </nav>
